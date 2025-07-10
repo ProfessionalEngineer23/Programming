bd78b4ab6bd0618d59d38ce671297be498de7fdb
Niki <nikitamardari@gmail.com>
2025-07-10 23:55:16 +0100

    Basic condition in Python

---
+++ Python/conditions.py
@@ -0,0 +1,12 @@
+# Condition statement example
+
+album_year = 1983
+#album_year = 1970
+
+if album_year > 1980:
+    print("Album year is greater than 1980")
+else:
+    print("less than 1980")
+
+print('do something..')
+
