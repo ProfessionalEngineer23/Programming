db62b5157554d4ba5491068a1b5ae791388ca62a
Niki <nikitamardari@gmail.com>
2025-07-10 23:56:37 +0100

    Update conditions.py

--- Python/conditions.py
+++ Python/conditions.py
@@ -5,6 +5,8 @@ album_year = 1983
 
 if album_year > 1980:
     print("Album year is greater than 1980")
+elif album_year == 1980:
+    print("The year is exactly 1980!")
 else:
     print("less than 1980")
 
